002b61167b3f06620a98e1583aa5d7cab0da1356
Grzegorz <g.w.mika@gmail.com>
2020-01-31 07:11:05 +0100

    add retry to GCP upload

--- collect_data/Collect_data.py
+++ collect_data/Collect_data.py
@@ -16,7 +16,8 @@ API_key = Path('/home/airly/Airly_API_key.txt').read_text().split('\n')[0]
 latitude = 50.061464
 longitude = 19.938000
 max_distance_km = 100
-
+max_tries = 3
+load_break = 5
 
 miner = AirlyDownloader.AirlyDownloader(API_key)
 ids = miner.get_installations_ids_location(latitude, longitude, max_distance_km)
@@ -36,10 +37,16 @@ f = StringIO()
 data.to_csv(f, index_label=False)
 f.seek(0)
 
-storage_client = storage.Client.from_service_account_json('/home/data/secretgc.json')
+for i in range(max_tries):
+    try:
+        time.sleep(load_break)
+        storage_client = storage.Client.from_service_account_json('/home/data/secretgc.json')
+        bucket = storage_client.bucket('airly_data')
+        blob = bucket.blob(str(date.today()) + '.csv')
+        blob.upload_from_file(f, content_type='text/csv')
+        break
+    except Exception as err:
+        logging.error(err)
+        continue
 
-bucket = storage_client.bucket('airly_data')
-blob = bucket.blob(str(date.today()) + '.csv')
-
-blob.upload_from_file(f, content_type='text/csv')
 
